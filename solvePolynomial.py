c6b87a20bd913ab2be3018a3663c396e4d34adb6
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-06-16 23:04:13 -0400

    Derivitive child class added to Operation parent.

--- solvePolynomial.py
+++ solvePolynomial.py
@@ -12,22 +12,26 @@ class solvePolynomial:
         self.mult = Multiply(self.poly.order + 1)
         self.sub_space_hyper_sphere = hyperSphere(self.poly.order)
         self.iterations = iterations
-        self.o = self.optimize()
+        self.o, self.measure___ = self.optimize()
 
     def __call__(self):
         pass
 
     def optimize(self):
+        measure___ = []
         for i in range(self.iterations):
             domain_, sub_domain = self.sample_domain_sub_domain(domain_ if i > 0 else None)
-            domain_ball = self.domain_ball(sub_domain)
+            domain_ball = self.domain_ball(sub_domain, i+1)
             domain_, range_ = self.make_domain_range(domain_, domain_ball)
-            measure__index, domain_ = self.measure_(domain_, range_)
-        return domain_%2
+            measure__, measure__index, domain_ = self.measure_(domain_, range_)
+            if i > 0:
+                measure___.append(measure__)
+        measure___ = np.stack(measure___, axis = 0)
+        return domain_%2, measure___
 
     def sample_domain_sub_domain(self, domain = None, samples = 100):
         if isinstance(domain, type(None)):
-            domain = 2*np.pi*np.random.random_sample(size=(samples, self.mult.dims-1))
+            domain = 2*np.pi*np.random.random_sample(size=(10000, self.mult.dims-1))
         sub_domain = 2*np.pi*np.random.random_sample(size=(samples, self.mult.dims-2))
         return domain, sub_domain
 
@@ -46,8 +50,15 @@ class solvePolynomial:
         measure__ = ((range_ @ self.normal_vector) - 1)**2
         measure__index = np.argpartition(measure__, n)
         print(measure__[measure__index[:n]])
-        return measure__index, domain_[measure__index[:n]]
+        return measure__[measure__index[:n]], measure__index, domain_[measure__index[:n]]
+
+    def plot_measure(self):
+        import matplotlib.pyplot as plt
+        for i, j in enumerate(self.measure___):
+            for k in j:
+                plt.scatter(i/(self.iterations*100),k)
+        plt.show()
 
 if __name__ == "__main__":
-    pol = Polynomial(np.array([5,1,2,3,10,1]))
+    pol = Polynomial(np.random.random_sample(7))
     sP = solvePolynomial(pol)
